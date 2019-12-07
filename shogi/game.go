591a6c5645dcc8cb9eaadee2174ee39f1fbe5291
kanji-yomoda <kanji-yomoda@m3.com>
2019-12-07 15:28:05 +0900

    Remove unused function

--- shogi/game.go
+++ shogi/game.go
@@ -49,25 +49,3 @@ func (g *Game) switchPlayer() {
 	}
 }
 
-func (g *Game) isMovable(curPos, distPos *Position) bool {
-	piece, exist := g.board.FindPiece(curPos)
-	if !exist {
-		fmt.Println(fmt.Sprintf("piece doesn't exist at %v", curPos))
-		return false
-	}
-	if !IsSamePlayer(g.currentPlayer, piece.Owner()) {
-		fmt.Println(fmt.Sprintf("piece doesn't belong to %s", piece.Owner().Name()))
-		return false
-	}
-	distPositionPiece, exist := g.board.FindPiece(distPos)
-	if exist && IsSamePlayer(g.currentPlayer, distPositionPiece.Owner()) {
-		fmt.Println(fmt.Sprintf("there is current user's piece at %v", distPos))
-		return false
-	}
-	if !piece.IsMovableTo(curPos, distPos) {
-		fmt.Println(fmt.Sprintf("the piece can't be moved to %v", distPos))
-		return false
-	}
-	g.board[distPos.Y][distPos.X] = g.board[curPos.Y][curPos.X]
-	return true
-}
